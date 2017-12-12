be336a4eab9300188f0a1295e55c7131bb8c96b8
pagers <piotr.grzela@op.pl>
2017-12-12 01:03:12 +0100

    eee

--- functions.php
+++ functions.php
@@ -1,47 +1,57 @@
-<?php
-$dbhost = 'localhost';
-$dbname = 'appsecurity';
-$dbuser = 'root';
-$dbpass = '';
-$appname = 'SecurityApps';
-$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-if ($connection->connect_error) die($connection->connect_error);
-function createTable($name, $query)
-{
-    queryMysql("CREATE TABLE IF NOT EXISTS  $name($query)");
-    echo  "Tabela '$name' została utworzona lub już istnieje";
-}
-function queryMysql($query)
-{
+<?php // Przykład 26-1: functions.php
+  $dbhost  = 'localhost';    // To raczej nie będzie wymagało zmiany
+  $dbname  = 'appsecurity';   // Zmodyfikuj te...
+  $dbuser  = 'root';   // ...zmienne zgodnie
+  $dbpass  = '';   // ...z parametrami instalacji
+  $appname = "Security Apps"; // ...i własnymi preferencjami
+
+  $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($connection->connect_error) die($connection->connect_error);
+
+  function createTable($name, $query)
+  {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Tabela '$name' została utworzona lub już istnieje.<br>";
+  }
+
+  function queryMysql($query)
+  {
     global $connection;
     $result = $connection->query($query);
     if (!$result) die($connection->error);
     return $result;
-}
-function destroySession()
-{
+  }
+
+  function destroySession()
+  {
     $_SESSION=array();
+
     if (session_id() != "" || isset($_COOKIE[session_name()]))
-        setcookie(session_name(), '', time()-2592000, '/');
+      setcookie(session_name(), '', time()-2592000, '/');
+
     session_destroy();
-}
-function sanitizeString($var)
-{
+  }
+
+  function sanitizeString($var)
+  {
     global $connection;
     $var = strip_tags($var);
     $var = htmlentities($var);
     $var = stripslashes($var);
     return $connection->real_escape_string($var);
-}
-function showProfile($user)
-{
+  }
+
+  function showProfile($user)
+  {
     if (file_exists("$user.jpg"))
-        echo "<img src='$user.jpg' style='float:left;'>";
+      echo "<img src='$user.jpg' style='float:left;'>";
+
     $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
     if ($result->num_rows)
     {
-        $row = $result->fetch_array(MYSQL_ASSOC);
-        echo stripslashes($row['text']). "<br style='clear:left;'><br/>";
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
     }
-}
+  }
 ?>
--- header.php
+++ header.php
@@ -1,34 +1,44 @@
-<?php
-session_start();
-echo "<!DOCTYPE html>\n<html><head><meta charset='UTF-8'";
-require_once 'functions.php';
-$userstr = '(Gość)';
-if (isset($_SESSION['user']))
-{
-    $user = $_SESSION['user'];
-    $loggin = TRUE;
-    $userstr = ($user);
-}
-else $loggin = FALSE;
-echo "<title>$appname$userstr</title><link rel = 'stylesheet'" .
-    "href='style.css' type='text/css'>" .
-    "</head><body><center><canvas id='logo'>with='624'" .
-    "height='96'>$appname</canvas></center>" .
-    "<div class ='appname'>$appname$userstr</div>" .
-    "<script src='javascript.js'></script>";
-if ($loggin)
+<?php // Przykład 26-2: header.php
+  session_start();
+
+  echo "<!DOCTYPE html>\n<html><head><meta charset='utf-8'>";
+
+  require_once 'functions.php';
+
+  $userstr = ' (Gość)';
+
+  if (isset($_SESSION['user']))
+  {
+    $user     = $_SESSION['user'];
+    $loggedin = TRUE;
+    $userstr  = " ($user)";
+  }
+  else $loggedin = FALSE;
+
+  echo "<title>$appname$userstr</title><link rel='stylesheet' " .
+       "href='styles.css' type='text/css'>"                     .
+       "</head><body><center><canvas id='logo' width='624' "    .
+       "height='96'>$appname</canvas></center>"             .
+       "<div class='appname'>$appname$userstr</div>"            .
+       "<script src='javascript.js'></script>";
+
+  if ($loggedin)
+  {
     echo "<br ><ul class='menu'>" .
-        "<li><a href='members.php?view=$user'>Główna</a></li>" .
-        "<li><a href='members.php'>Użytkownicy</a></li>" .
-        "<li><a href='friends.php'>Znajomi</a></li>" .
-        "<li><a href='messages.php'>Wiadomości</a></li>" .
-        "<li><a href='profile.php'>Profil</a></li>" .
-        "<li><a href='logout.php'>Wyloguj</a></li>";
-else
-    echo ("<br><ul class = 'menu'>") .
-        "<li><a href='index.php'Strona Główna</a></li>" .
-        "<li><a href='signup.php'Zarejestruj się</a></li>" .
-        "<li><a href='login.php'Zaloguj się</a></li></ul><br>" .
-        "<span class = 'info'>$#8658; Musisz być zalogowany, aby " .
-        "wyświetlić te stronę. </span><br><br>)";
+         "<li><a href='members.php?view=$user'>Główna</a></li>" .
+         "<li><a href='members.php'>Użytkownicy</a></li>"         .
+         "<li><a href='friends.php'>Znajomi</a></li>"         .
+         "<li><a href='messages.php'>Wiadomości</a></li>"       .
+         "<li><a href='profile.php'>Profil</a></li>"    .
+         "<li><a href='logout.php'>Wyloguj</a></li></ul><br>";
+  }
+  else
+  {
+    echo ("<br><ul class='menu'>" .
+          "<li><a href='index.php'>Strona główna</a></li>"                .
+          "<li><a href='signup.php'>Zarejestruj się</a></li>"            .
+          "<li><a href='login.php'>Zaloguj się</a></li></ul><br>"     .
+          "<span class='info'>&#8658; Musisz być zalogowany, aby " .
+          "wyświetlić tę stronę.</span><br><br>");
+  }
 ?>
